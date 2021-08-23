72619378e545aaddda0ea9752544f88e939e45e1
Ana Paula dos Santos Buchholz <ana.buchholz@dtidigital.com.br>
2021-08-23 11:28:06 -0300

    Commit Inicial

---
+++ AppControleRenda/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppControleRenda.Controllers
+{
+    public class Message
+    {
+        public int id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+}
